2c421820529ca58ff3e1610ce356b658f8ae9945
ISxaNI <jasonliu99999@yahoo.com>
2024-02-09 22:09:29 -0800

    FINALLY

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,38 @@
+async function main() {
+    let books = await fetch('http://localhost:3001/listBooks')
+    let booksJSON = await books.json()
+    
+    booksJSON.forEach(function(book) {
+        let div = document.createElement('div')
+        div.innerHTML = `
+            <img src="${book.imageURL}" width="200" />
+            <h3 id="title-${book.id}">${book.title}</h3>
+            <p>Year Published: ${book.year}</p>
+            <p>Quantity: ${book.quantity}</p>
+            <input id="${book.id}" type= "text" />
+            <input type= "submit" onclick ="changeTitle(${book.id})" />
+        `
+        document.body.append(div)
+    })
+}
+main()
 
-// Your Code Here
+async function changeTitle(id) {
+    let input = document.getElementById(id)
+    let value = input.value
+
+    let response = await fetch('http://localhost:3001/updateBook', {
+        method: 'PATCH',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            id: id,
+            title: value,
+        })
+    })
+    let responseJSON = await response.json()
+    console.log(response.json)
+
+    document.getElementById(`title-${id}`).textContent = value
+}
